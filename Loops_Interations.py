53fa0f96c8cc48266b7292cce1354794f8b15cc5
quanduong0250 <76028538+quanduong0250@users.noreply.github.com>
2020-12-17 05:32:35 +0700

    Add files via upload
    test upload file to Github

---
+++ Loops_Interations.py
@@ -0,0 +1,35 @@
+#Largest
+
+the_smallest = 7
+print (the_smallest,"Before Run")
+for the_num in [11,13,9,21,78,1,4] :
+    if the_num < the_smallest :
+        the_smallest = the_num
+    print (the_smallest,the_num)
+print (the_smallest,"After Run")
+print("\n")
+
+# đếm số _ counting
+
+STT = 0
+print("Chưa đếm thấy" , STT)
+for dem_so in [9,41,12,3,74,15] :
+    STT = STT + 1
+    print (STT , dem_so)
+print ("Sau khi đếm thấy" , STT)
+print("\n")
+
+# Total
+
+Tong_So = 0
+print ("Before" , Tong_So)
+for Cong_Vao in [9,41,12,3,74,15] :
+    Tong_So = Tong_So + Cong_Vao
+    print (Cong_Vao , Tong_So)
+print ("After" , Tong_So)
+print("\n")
+
+found = None
+print (found)
+for value in [9,41,12,3,74,15]
+    if 
